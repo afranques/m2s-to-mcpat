79b2282c9ec20e6d771f82bc290df9239552cc2f
afranques <franques.antonio@gmail.com>
2017-07-10 07:25:14 -0500

    it finds all sections and all the params within

--- m2s-to-mcpat.py
+++ m2s-to-mcpat.py
@@ -4,17 +4,32 @@ import re
 # regexp to find the title of a section, like "[ Config.General ]"
 section_title_regex = re.compile( r"\[ ([^\s\]]+) \]" )
 
+# global variable to keep track of what section we are parsing now
+current_section = None
+
 def parser( line, results_sections ):
+    global current_section
+
+    # if we find a section title
     section_title = section_title_regex.match( line )
     if section_title:
-        print section_title.group(1)
+        # DEBUG: print section_title.group(1)
+        # we create a new pair into the results_sections dictionary, where
+        # the key is the section title and the value is another dictionary,
+        # which will contain all pairs of parameters/values for that section
+        results_sections[section_title.group(1).strip()] = {}
+        current_section = section_title.group(1).strip() # we update the current section name
+    # otherwise we are still inside a section
     else:
         try:
             param_name, param_value = line.split("=")
         except ValueError:
             pass
         else:
-            print param_name.strip(), param_value.strip()
+            # DEBUG: print param_name.strip(), param_value.strip()
+            if current_section is not None:
+                results_sections[current_section][param_name.strip()] = param_value.strip()
+                
 
 
 
@@ -28,8 +43,11 @@ if __name__ == '__main__': #this is how the main function is called in python
     # Command to execute this code: shell> python <nameOfThisFile>.py <m2sInputFile> <mcpatTemplateFile> <mcpatOutputFile>
     # This program takes a Multi2Sim results file, it reads the components it needs for McPat, it makes a copy of the mcpat template file
     # and it overwrites that copy with the components indicated.
-    filename = sys.argv[1] #the argument of the python call will be the name of the source code file
-    results_sections = {}
+
+    filename = sys.argv[1] # first argument is m2sInputFile
+    results_sections = {} # this dictionary will contain one row per every section in the m2s results file
+
+    # we read m2sInputFile one line at a time (this will automatically close the file at the end)
     with open(filename) as infile:
         for line in infile:
             parser(line, results_sections)
