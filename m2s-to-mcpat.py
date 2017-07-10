877cdf200caeb21396f09b76b3a62bd28a83f302
afranques <franques.antonio@gmail.com>
2017-07-10 07:37:51 -0500

    changed name of dictionary to be more intuitive

--- m2s-to-mcpat.py
+++ m2s-to-mcpat.py
@@ -4,32 +4,34 @@ import re
 # regexp to find the title of a section, like "[ Config.General ]"
 section_title_regex = re.compile( r"\[ ([^\s\]]+) \]" )
 
-# global variable to keep track of what section we are parsing now
+# global variable to keep track of what section are we parsing now
 current_section = None
 
-def parser( line, results_sections ):
+def parser( line, m2s_sections ):
     global current_section
 
     # if we find a section title
     section_title = section_title_regex.match( line )
     if section_title:
         # DEBUG: print section_title.group(1)
-        # we create a new pair into the results_sections dictionary, where
+        # we create a new pair into the m2s_sections dictionary, where
         # the key is the section title and the value is another dictionary,
         # which will contain all pairs of parameters/values for that section
-        results_sections[section_title.group(1).strip()] = {}
+        m2s_sections[section_title.group(1).strip()] = {}
         current_section = section_title.group(1).strip() # we update the current section name
     # otherwise we are still inside a section
     else:
         try:
+            # this will fail if there's no pair of "param = value" in the line
             param_name, param_value = line.split("=")
         except ValueError:
             pass
         else:
             # DEBUG: print param_name.strip(), param_value.strip()
             if current_section is not None:
-                results_sections[current_section][param_name.strip()] = param_value.strip()
-                
+                # we define a new entry where the key is the name of the parameter, and we also assign its value
+                m2s_sections[current_section][param_name.strip()] = param_value.strip()
+
 
 
 
@@ -45,9 +47,9 @@ if __name__ == '__main__': #this is how the main function is called in python
     # and it overwrites that copy with the components indicated.
 
     filename = sys.argv[1] # first argument is m2sInputFile
-    results_sections = {} # this dictionary will contain one row per every section in the m2s results file
+    m2s_sections = {} # this dictionary will contain one row per every section in the m2s results file
 
     # we read m2sInputFile one line at a time (this will automatically close the file at the end)
     with open(filename) as infile:
         for line in infile:
-            parser(line, results_sections)
+            parser(line, m2s_sections)
