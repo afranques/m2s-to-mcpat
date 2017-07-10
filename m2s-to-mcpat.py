a3a630df43fa7a05356af407d23b1324be3ae004
afranques <franques.antonio@gmail.com>
2017-07-10 10:13:58 -0500

    added skeleton for filler function

--- m2s-to-mcpat.py
+++ m2s-to-mcpat.py
@@ -10,6 +10,9 @@ section_title_regex = re.compile( r"\[ ([^\s\]]+) \]" )
 current_section = None
 
 # correspondences between McPat and Multi2Sim (A.K.A. m2s) statistics
+# all parameters in the left column of this list will be tried to be
+# replaced in the mcpat template for their equivalent parameters in m2s (right column)
+# MODIFY THIS LIST IN ORDER TO ADAPT THIS PROGRAM TO YOUR OWN NEEDS
 corresp_mcpat_to_m2s = [
     ("system->total_cycles",                            "global->cycles"),
     ("system->busy_cycles",                             "global->cycles"),
@@ -19,6 +22,9 @@ corresp_mcpat_to_m2s = [
     # ("",""),
 ]
 
+# The parser is called on every line of the m2sInputFile, and it dynamically creates
+# a dictionary where every entry is a section of the m2s file, and every value is
+# another dictionary that contains all the parameters and values for that section
 def parser( line, m2s_sections ):
     global current_section
 
@@ -44,8 +50,11 @@ def parser( line, m2s_sections ):
                 # we define a new entry where the key is the name of the parameter, and we also assign its value
                 m2s_sections[current_section][param_name.strip().lower()] = param_value.strip().lower()
 
-
-
+# The filler is called on every line of the mcpatTemplateFile. If the line doesn't contain a
+# parameter from corresp_mcpat_to_m2s that needs to be filled with its corresponding value from m2s, it is
+# simply copied from mcpatTemplateFile to mcpatOutputFile, otherwise its value is obtained from m2s_sections
+def filler( line, mcpatOutputFile):
+    
 
 
 
@@ -54,14 +63,26 @@ def parser( line, m2s_sections ):
 
 
 if __name__ == '__main__': #this is how the main function is called in python
-    # Command to execute this code: shell> python <nameOfThisFile>.py <m2sInputFile> <mcpatTemplateFile> <mcpatOutputFile>
+    # Command to execute this code: shell> python m2s-to-mcpat.py <m2sInputFile> <mcpatTemplateFile.xml> <mcpatOutputFile.xml>
     # This program takes a Multi2Sim results file, it reads the components it needs for McPat, it makes a copy of the mcpat template file
-    # and it overwrites that copy with the components indicated.
+    # and it overwrites that copy with the components indicated. The template file should have the structure of each component and the list
+    # of parameters that we're trying to copy from m2s. If any of the components is not found on the template (mcpatTemplateFile), the parsing will fail.
 
-    filename = sys.argv[1] # first argument is m2sInputFile
+    m2sInputFileName = sys.argv[1] # first argument is the m2s results output file from where we'll read the values
+    mcpatTemplateFileName = sys.argv[2] # second argument is the mcpat template that we're trying to fill (this file won't be modified)
+    mcpatOutputFileName = sys.argv[3] # third argument is the actual output file, ready to be used as input XML in mcpat
     m2s_sections = {} # this dictionary will contain one row per every section in the m2s results file
 
     # we read and parse m2sInputFile one line at a time (this will automatically close the file at the end)
-    with open(filename) as infile:
-        for line in infile:
+    with open(m2sInputFileName) as m2sInputFile:
+        for line in m2sInputFile:
             parser(line, m2s_sections)
+
+    mcpatOutputFile = open(mcpatOutputFileName,"w+") # we create the resulting output file name
+
+    # we read and parse m2sInputFile one line at a time (this will automatically close the file at the end)
+    with open(mcpatTemplateFileName) as mcpatTemplateFile:
+        for line in mcpatTemplateFile:
+            filler(line, mcpatOutputFile)
+
+    mcpatOutputFile.close() # once we've finished reading all the mcpatTemplateFile, we close the output file and we're done.
